ad4d352299c76d5951d3a07ab148b4f27ba59ed0
marc <marcrmasso.developer@gmail.com>
2025-08-02 13:38:03 +0200

    Tres primeres lliçons completades

--- 02-tipatge.js
+++ 02-tipatge.js
@@ -1,6 +1,6 @@
 //Cadena de text (String)
-let name = "Marc RM"
-let alias = 'Marc Prog'
+let myName = "Marc"
+let alias = 'MarcProg'
 let email = `marcrmasso.developer@gmail.com`
 
 //números (number)
@@ -13,11 +13,9 @@ let isTeacher = true
 
 //Undefined
 let variableIndefinida
-console.log(variableIndefinida)
 
 //Null
 let valorNull = null
-console.log(valorNull)
 
 //Symbol
 let mySymbol = Symbol("mysymbol")
@@ -25,3 +23,17 @@ let mySymbol = Symbol("mysymbol")
 //BigInt
 let myBigInt = BigInt(2348739473924739247430473024307432)
 let myBigInt2 = 2348739473924739247430473024307432n
+
+//Mostrem els tipus de dades
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+console.log(typeof age)
+console.log(typeof height)  
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+console.log(typeof variableIndefinida)
+console.log(typeof valorNull)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
